c9f722231259d61b253d1ed49f332182474979fa
Yevgeniy Firsov <firsov@tigrisdata.com>
2022-03-09 17:46:00 -0800

    Test and refactor iterator

--- client/driver/iterator.go
+++ client/driver/iterator.go
@@ -3,8 +3,8 @@ package driver
 import "io"
 
 type Iterator interface {
-	More() bool
-	Next() (Document, error)
+	Next(d *Document) bool
+	Err() error
 }
 
 type streamReader interface {
@@ -14,37 +14,29 @@ type streamReader interface {
 type readIterator struct {
 	streamReader
 	eof bool
-	doc Document
 	err error
 }
 
-func (i *readIterator) More() bool {
+func (i *readIterator) Next(d *Document) bool {
 	if i.eof {
 		return false
 	}
-	// document or error from previous call wasn't popped yet
-	if i.doc != nil || i.err != nil {
-		return true
-	}
+
 	doc, err := i.read()
 	if err == io.EOF {
 		i.eof = true
 		return false
 	}
 	if err != nil {
+		i.eof = true
 		i.err = err
-		return true
+		return false
 	}
-	i.doc = doc
+
+	*d = doc
 	return true
 }
 
-func (i *readIterator) Next() (Document, error) {
-	if i.err != nil {
-		i.eof = true
-		return nil, i.err
-	}
-	d := i.doc
-	i.doc = nil
-	return d, nil
+func (i *readIterator) Err() error {
+	return i.err
 }

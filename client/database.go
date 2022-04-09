a2522e5b8e9dc72a11a3f2b681479accc1fd4e1d
Richard Artoul <richardartoul@gmail.com>
2022-04-09 16:36:12 -0500

    implement ApplySchemasFromDirectory method

--- client/database.go
+++ client/database.go
@@ -2,7 +2,9 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/tigrisdata/tigrisdb-client-go/driver"
 )
@@ -41,6 +43,59 @@ func (d *database) Run(
 	return res, nil
 }
 
-func (d *database) ApplySchemasFromDirectory(path string) error {
-	panic("not implemented")
+func (d *database) ApplySchemasFromDirectory(ctx context.Context, path string) error {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return fmt.Errorf(
+			"error reading schemas directory: %s, err: %w", path, err)
+	}
+
+	type schema struct {
+		name  string
+		bytes []byte
+	}
+
+	type collectionName struct {
+		Name string `json:"name"`
+	}
+
+	schemas := map[string]schema{}
+	for _, f := range files {
+		fBytes, err := ioutil.ReadFile(f.Name())
+		if err != nil {
+			return fmt.Errorf(
+				"error reading schema from file: %s, err: %w",
+				f.Name(), err)
+		}
+
+		name := collectionName{}
+		if err := json.Unmarshal(fBytes, &name); err != nil {
+			return fmt.Errorf(
+				"error parsing JSON for schema in file: %s, err: %w",
+				f.Name(), err)
+		}
+		if name.Name == "" {
+			return fmt.Errorf(
+				"did not find collection name in schema file: %s", f.Name())
+		}
+
+		schemas[f.Name()] = schema{
+			name:  name.Name,
+			bytes: fBytes,
+		}
+	}
+
+	_, err = d.Run(ctx, func(ctx context.Context, tx driver.Tx) (interface{}, error) {
+		for _, schema := range schemas {
+			err := tx.CreateOrUpdateCollection(
+				ctx, schema.name, driver.Schema(schema.bytes))
+			if err != nil {
+				return nil, fmt.Errorf(
+					"error applying schema for collection: %s, err: %w", schema.name, err)
+			}
+		}
+
+		return nil, nil
+	})
+	return err
 }
